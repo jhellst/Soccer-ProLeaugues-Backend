cbcf4b22c5b512aabbe3c45dd23ebf503816483d
Joshua <jhellst@gmail.com>
2024-11-08 17:47:22 -0800

    Uncommented run command.

--- app.py
+++ app.py
@@ -448,5 +448,8 @@ def page_not_found(e):
 # if __name__ == '__main__':
 #     app.run(port=5001, debug=True)
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='4000')
+
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port='4000')
